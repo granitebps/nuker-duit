bf0022c3206e1e58fbcd1a50e72d5a233d16088d
Granite Bagas <granitebagas28@gmail.com>
2023-12-13 23:13:02 +0700

    Add api helper and customize error handler

--- api/app/Exceptions/Handler.php
+++ api/app/Exceptions/Handler.php
@@ -2,7 +2,18 @@
 
 namespace App\Exceptions;
 
+use App\Helpers\Helper;
+use Exception;
+use Illuminate\Auth\Access\AuthorizationException;
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Http\Response;
+use Illuminate\Validation\ValidationException;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
+use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -26,5 +37,76 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+        $this->renderable(function (Exception $e) {
+            return $this->handleException($e);
+        });
+    }
+
+    /**
+     * Check and Handle somes Exception
+     *
+     * @param Exception $exception
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    private function handleException(Exception $exception)
+    {
+        if ($exception instanceof NotFoundHttpException) {
+            if ($exception->getPrevious() instanceof ModelNotFoundException) {
+                return Helper::apiErrorResponse('Data Not Found', Response::HTTP_NOT_FOUND);
+            }
+            return Helper::apiErrorResponse('Endpoint Not Found', Response::HTTP_NOT_FOUND);
+        }
+
+        if ($exception instanceof ValidationException) {
+            return Helper::apiErrorResponse('Given Data is Invalid', Response::HTTP_UNPROCESSABLE_ENTITY, $this->transformErrors($exception));
+        }
+
+        if ($exception instanceof MethodNotAllowedHttpException) {
+            return Helper::apiErrorResponse('Method Not Allowed', Response::HTTP_METHOD_NOT_ALLOWED);
+        }
+
+        if ($exception instanceof AccessDeniedHttpException) {
+            return Helper::apiErrorResponse('Access Denied', Response::HTTP_FORBIDDEN);
+        }
+
+        if ($exception instanceof AuthenticationException) {
+            return Helper::apiErrorResponse('Unauthenticated', Response::HTTP_UNAUTHORIZED);
+        }
+
+        if ($exception instanceof AuthorizationException) {
+            return Helper::apiErrorResponse('Unauthenticated', Response::HTTP_UNAUTHORIZED);
+        }
+
+        if ($exception instanceof HttpExceptionInterface) {
+            return Helper::apiErrorResponse($exception->getMessage(), $exception->getStatusCode());
+        }
+
+        return Helper::apiErrorResponse(
+            app()->isProduction() ? 'Internal Server Error' : $exception->getMessage(),
+            Response::HTTP_INTERNAL_SERVER_ERROR
+        );
+    }
+
+    /**
+     * Undocumented function
+     *
+     * @param ValidationException $exception
+     *
+     * @return Array
+     */
+    private function transformErrors(ValidationException $exception)
+    {
+        $errors = [];
+        if (is_array($exception->errors())) {
+            foreach ($exception->errors() as $field => $message) {
+                $errors[] = [
+                    'field' => $field,
+                    'message' => $message[0],
+                ];
+            }
+        }
+
+        return $errors;
     }
 }
---
+++ api/app/Helpers/helper.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Helpers;
+
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Http\Response;
+
+class Helper
+{
+    /**
+     * @param string $message
+     * @param mixed $result
+     * @return JsonResponse
+     */
+    public static function apiResponse(string $message = 'Success', mixed $result = []): JsonResponse
+    {
+        if ($result instanceof JsonResource) {
+            $response = $result->additional([
+                'success' => true,
+                'message' => $message,
+            ]);
+            return ($response)->response()->setStatusCode(Response::HTTP_OK);
+        } else {
+            $response = [
+                'success' => true,
+                'message' => $message,
+                'data' => $result,
+            ];
+            return response()->json($response, Response::HTTP_OK);
+        }
+    }
+
+    /**
+     * @param string $message
+     * @param int $code
+     * @param array $data
+     * @return JsonResponse
+     */
+    public static function apiErrorResponse(string $message = 'Internal Server Error', int $code = Response::HTTP_BAD_REQUEST, array $data = []): JsonResponse
+    {
+        return response()->json([
+            'success' => false,
+            'message' => $message,
+            'data' => $data
+        ], $code);
+    }
+}
